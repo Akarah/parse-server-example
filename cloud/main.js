c5b96743197311374decf2859b77e935e3ffae2d
Sandro <sandro@ary.travel>
2020-11-03 12:11:19 +0100

    server time format

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,8 @@ var moment = require('moment');
 
 Parse.Cloud.define("getServerTime", function(request, response) {
 
-  let tz = momenttimezone().format().substr(id.length - 5)
+  let tz = momenttimezone().format();
+  tz = tz.substr(tz.length - 5)
 
   return moment().format("YYYY-MM-DDTHH:mm:ss.SSS") + tz;
 });
